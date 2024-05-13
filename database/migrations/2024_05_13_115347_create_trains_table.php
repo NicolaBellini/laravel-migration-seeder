6ea5c9513a068df5a338d2dfaa43f0ed90c3e138
NicolaBellini <bellininicola4366@gmail.com>
2024-05-13 14:19:38 +0200

    [add[ migration trains

--- database/migrations/2024_05_13_115347_create_trains_table.php
+++ database/migrations/2024_05_13_115347_create_trains_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->date('orario_di_partenza');
+            $table->date('orario_di_arrivo');
+            $table->string('slug',100)->unique();
+            $table->string('codice_treno', 20);
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
             $table->timestamps();
         });
     }
