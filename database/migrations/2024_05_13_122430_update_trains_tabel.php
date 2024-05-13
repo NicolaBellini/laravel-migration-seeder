2e2fc6a69fc0864f49b227bf990ad96e491d4c6c
NicolaBellini <bellininicola4366@gmail.com>
2024-05-13 14:29:50 +0200

    [add] after( ) in update

--- database/migrations/2024_05_13_122430_update_trains_tabel.php
+++ database/migrations/2024_05_13_122430_update_trains_tabel.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('trains', function (Blueprint $table) {
-            $table->boolean('regionale')->default(0);
+            $table->boolean('regionale')->default(0)->after('azienda');
         });
     }
 
