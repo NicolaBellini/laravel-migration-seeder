0579ce5cd82cac3256271350f968ecf9578beb98
NicolaBellini <bellininicola4366@gmail.com>
2024-05-14 14:28:07 +0200

    [add] for loop in seeder

--- database/seeders/trainsSeederTable.php
+++ database/seeders/trainsSeederTable.php
@@ -15,20 +15,23 @@ class trainsSeederTable extends Seeder
      */
     public function run(Faker $faker): void
     {
+        for($i=0;$i<100;$i++){
 
-        $train= new train;
-        $train->azienda= $faker->company();
-        $train->stazione_di_partenza= $faker->city();
-        $train->stazione_di_arrivo= $faker->city();
-        $train->orario_di_partenza= $faker->time();
-        $train->orario_di_arrivo= $faker->time();
-        $train->codice_treno= $faker->randomNumber(5,true);
-        $train->numero_carrozze= $faker->numberBetween(5, 20);
-        $train->in_orario= $faker->boolean();
-        $train->cancellato= $faker->boolean();
-        $train->slug= $this->getSlugUnivoque($train->azienda,$train->codice_treno);
+            $train= new train;
+            $train->azienda= $faker->company();
+            $train->stazione_di_partenza= $faker->city();
+            $train->stazione_di_arrivo= $faker->city();
+            $train->orario_di_partenza= $faker->time();
+            $train->orario_di_arrivo= $faker->time();
+            $train->codice_treno= $faker->randomNumber(5,true);
+            $train->numero_carrozze= $faker->numberBetween(5, 20);
+            $train->in_orario= $faker->boolean();
+            $train->cancellato= $faker->boolean();
+            $train->slug= $this->getSlugUnivoque($train->azienda,$train->codice_treno);
 
-        $train->save();
+            $train->save();
+        }
+
     }
 
     private function getSlugUnivoque($company, $code){
