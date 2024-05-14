fd3550e03bb0622e2632fff17b8542cdc8f72d00
NicolaBellini <bellininicola4366@gmail.com>
2024-05-14 15:58:48 +0200

    [add] csv seeder while loop with $row dump

--- database/seeders/trainstTableSeederCsv.php
+++ database/seeders/trainstTableSeederCsv.php
@@ -7,15 +7,26 @@ use Illuminate\Database\Seeder;
 use App\Models\train;
 use Illuminate\Support\Str;
 
+use function PHPUnit\Framework\assertFalse;
+
 class trainstTableSeederCsv extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+      public function run(): void
     {
-        $data_str= fopen(__DIR__.'/trains.csv', 'r');
+        $data_str = fopen(__DIR__.'/trains.csv', 'r');
 
-        dump($data_str);
+        $i = 0;
+        while (($row = fgetcsv($data_str)) !== false) {
+            if ($i > 0) {
+                dump($row);
+
+            }
+            $i++;
+        }
+
+        fclose($data_str);
     }
 }
